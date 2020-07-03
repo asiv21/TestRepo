ff8fb05187fa4e826b12af5c7cc84df8a7b30226
Abhishek Sivaram <as5397@columbia.edu>
2020-07-03 01:39:03 -0400

    complex extension

--- Binet/binet.py
+++ Binet/binet.py
@@ -3,12 +3,15 @@ import math
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
 plt.rc("font", family='Arial', size=12)
 plt.rc("mathtext", fontset='stix')
 
 
 def binet(n):
     return np.power((1+np.sqrt(5))/2 + 0j,n)/np.sqrt(5) - np.power((1-np.sqrt(5))/2+ 0j,n)/np.sqrt(5)
+
 
 
 def complex_plot_(x, y, xlabel="x", ylabel="y", addtitle=" "):
@@ -38,23 +41,59 @@ def complex_plot_(x, y, xlabel="x", ylabel="y", addtitle=" "):
 	plt.suptitle(addtitle)
 
 	plt.tight_layout()
-	
+
+def complex_plot_2d_(X, Y, Z):
+	sk = 1
+	fig = plt.figure()
+	ax1 = fig.add_subplot(211, projection='3d')
+	ax1.plot_surface(X[::sk, ::sk], Y[::sk, ::sk], Z[::sk, ::sk].real)
+	ax1.set_xlabel(r"$x$")
+	ax1.set_ylabel(r"$iy$")
+	ax1.set_zlabel(r"Re($f(x+iy)$)")
+
+	ax2 = fig.add_subplot(212, projection='3d')
+	ax2.plot_surface(X[::sk, ::sk], Y[::sk, ::sk], Z[::sk, ::sk].imag)
+	ax2.set_xlabel(r"$x$")
+	ax2.set_ylabel(r"$iy$")
+	ax2.set_zlabel(r"Im($f(x+iy)$)")
+
+	plt.tight_layout()
 
 if __name__=="__main__":
-	print("Input value as x where interval is [-x, x]")
+	print("Input the upper bound of Benet numbers, n: ")
 	num = float(input())
 
+	####################################################
+	# Positive Numbers
+	####################################################
 	n_array = np.arange(0, num, 0.001)
 	binet_array = binet(n_array)
 	complex_plot_(n_array, binet_array, "$n$", "$f(n)$", "Positive numbers")	
 	
+	####################################################
+	# Negative Numbers
+	####################################################
 	n_array = np.arange(-num, 0, 0.001)
 	binet_array = binet(n_array)
 	complex_plot_(n_array, binet_array, "$n$", "$f(n)$", "Negative numbers")
 
+	####################################################
+	# Negative Numbers
+	####################################################
 	n_array = np.arange(-num, num, 0.001)
 	binet_array = binet(n_array)
-	complex_plot_(n_array, binet_array, "$n$", "$f(n)$")	
+	complex_plot_(n_array, binet_array, "$n$", "$f(n)$")
+
+
+	####################################################
+	# Complex Numbers
+	####################################################
+
+	X, Y = np.meshgrid(np.linspace(-num, num, 50), np.linspace(-num, num, 50))
+	Z = binet(X + Y*1j)
+	complex_plot_2d_(X, Y, Z)
+	print("Complex Analysis Done!")
+
 	plt.show()
 	
 	
