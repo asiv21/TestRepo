d65c72b78db598d7e33f4a36ac48dad3ef6572f6
Abhishek Sivaram <as5397@columbia.edu>
2020-07-04 00:23:06 -0400

    updating plots simultaneous

--- Binet/binet.py
+++ Binet/binet.py
@@ -15,21 +15,25 @@ def binet(n):
 
 
 def complex_plot_(x, y, xlabel="x", ylabel="y", addtitle=" "):
-	plt.figure(figsize=(3, 6))
+	plt.figure(figsize=(4, 8))
 	
 	plt.subplot(211)
 	plt.plot(x, np.real(y), linewidth=2, color='tab:blue', label=r"Re("+ylabel+")")
 	plt.plot(x, np.imag(y), linewidth=2, color='tab:red', label=r"Im("+ylabel+")")
 	plt.xlabel(xlabel)
+	plt.xticks(np.arange(x.min(), x.max()+1, 1))
+	plt.yticks(np.arange(y.real.min(), y.real.max()+1, 1))
 	plt.ylabel(r"Re, Im("+ylabel+")")
 	plt.title(" ")
 	plt.grid(which='major', axis='both')
 	plt.legend()
 
 	plt.subplot(212)
+	plt.axhline(0, color='lightgray')
 	plt.plot(np.real(y), np.imag(y), color='tab:green', linewidth=2)
 	plt.xlabel(r"Re("+ylabel+")")
 	plt.ylabel(r"Im("+ylabel+")")
+	plt.xticks(np.arange(y.real.min(), y.real.max()+1, 1))
 	plt.title(" ")
 	plt.grid(which='major', axis='both')
 
@@ -38,16 +42,15 @@ def complex_plot_(x, y, xlabel="x", ylabel="y", addtitle=" "):
 	plt.tight_layout()
 
 def complex_plot_2d_(X, Y, Z):
-	sk = 1
 	fig = plt.figure()
 	ax1 = fig.add_subplot(211, projection='3d')
-	ax1.plot_surface(X[::sk, ::sk], Y[::sk, ::sk], Z[::sk, ::sk].real, color='tab:blue')
+	ax1.plot_surface(X, Y, Z.real, color='tab:blue')
 	ax1.set_xlabel(r"$x$")
 	ax1.set_ylabel(r"$iy$")
 	ax1.set_zlabel(r"Re($f(x+iy)$)")
 
 	ax2 = fig.add_subplot(212, projection='3d')
-	ax2.plot_surface(X[::sk, ::sk], Y[::sk, ::sk], Z[::sk, ::sk].imag, color='tab:red')
+	ax2.plot_surface(X, Y, Z.imag, color='tab:red')
 	ax2.set_xlabel(r"$x$")
 	ax2.set_ylabel(r"$iy$")
 	ax2.set_zlabel(r"Im($f(x+iy)$)")
@@ -56,7 +59,12 @@ def complex_plot_2d_(X, Y, Z):
 
 if __name__=="__main__":
 	print("Input the upper bound of Binet numbers, n: ")
-	num = float(input())
+	num_str = input()
+	if num_str == "":
+		num_str = "6"
+	num = float(num_str)
+	print(num_str)
+
 
 	####################################################
 	# Positive Numbers
@@ -83,11 +91,10 @@ if __name__=="__main__":
 	####################################################
 	# Complex Numbers
 	####################################################
-
-	X, Y = np.meshgrid(np.linspace(-num, num, 50), np.linspace(-num, num, 50))
+	X, Y = np.meshgrid(np.linspace(-num, num, 100), np.linspace(0, num, 100))
 	Z = binet(X + Y*1j)
 	complex_plot_2d_(X, Y, Z)
-	print("Complex Analysis Done!")
+	print("Binet Analysis Done!")
 
 	plt.show()
 	
