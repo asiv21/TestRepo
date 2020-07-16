b27eeae28ea43e313abf3ca71e0319d08dc548e8
Abhishek Sivaram <as5397@columbia.edu>
2020-07-16 17:28:18 -0400

    colorcet for plotting

--- Binet/binet.py
+++ Binet/binet.py
@@ -4,10 +4,12 @@ import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+import colorcet as cc
 
 plt.rc("font", family='Arial', size=12)
 plt.rc("mathtext", fontset='stix')
-
+plt.rc("lines", linewidth=2.5)
+colormap = cc.cm.bkr
 
 def binet(n):
     return np.power((1+np.sqrt(5))/2 + 0j,n)/np.sqrt(5) - np.power((1-np.sqrt(5))/2+ 0j,n)/np.sqrt(5)
@@ -16,10 +18,10 @@ def binet(n):
 
 def complex_plot_(x, y, xlabel="x", ylabel="y", addtitle=" "):
 	plt.figure(figsize=(4, 8))
-	
+
 	plt.subplot(211)
-	plt.plot(x, np.real(y), linewidth=2, color='tab:blue', label=r"Re("+ylabel+")")
-	plt.plot(x, np.imag(y), linewidth=2, color='tab:red', label=r"Im("+ylabel+")")
+	plt.plot(x, np.real(y), color='tab:blue', label=r"Re("+ylabel+")")
+	plt.plot(x, np.imag(y), color='tab:red', label=r"Im("+ylabel+")")
 	plt.xlabel(xlabel)
 	plt.xticks(np.arange(x.min(), x.max()+1, 1))
 	plt.yticks(np.arange(y.real.min(), y.real.max()+1, 1))
@@ -30,10 +32,10 @@ def complex_plot_(x, y, xlabel="x", ylabel="y", addtitle=" "):
 
 	plt.subplot(212)
 	plt.axhline(0, color='lightgray')
-	plt.plot(np.real(y), np.imag(y), color='tab:green', linewidth=2)
+	plt.plot(np.real(y), np.imag(y), color='tab:green')
 	plt.xlabel(r"Re("+ylabel+")")
 	plt.ylabel(r"Im("+ylabel+")")
-	plt.xticks(np.arange(y.real.min(), y.real.max()+1, 1))
+	plt.xticks(np.arange(y.real.min(), y.real.max()+1, 2))
 	plt.title(" ")
 	plt.grid(which='major', axis='both')
 
@@ -44,13 +46,15 @@ def complex_plot_(x, y, xlabel="x", ylabel="y", addtitle=" "):
 def complex_plot_2d_(X, Y, Z):
 	fig = plt.figure()
 	ax1 = fig.add_subplot(211, projection='3d')
-	ax1.plot_surface(X, Y, Z.real, color='tab:blue')
+	divnorm1 = mpl.colors.DivergingNorm(vmin = -3, vmax=3, vcenter=0)
+	ax1.plot_surface(X, Y, Z.real, rstride=5, cstride=5, edgecolor='k', cmap=colormap, norm=divnorm1)
 	ax1.set_xlabel(r"$x$")
 	ax1.set_ylabel(r"$iy$")
 	ax1.set_zlabel(r"Re($f(x+iy)$)")
 
 	ax2 = fig.add_subplot(212, projection='3d')
-	ax2.plot_surface(X, Y, Z.imag, color='tab:red')
+	divnorm2 = mpl.colors.DivergingNorm(vmin = -3, vmax=3, vcenter=0)
+	ax2.plot_surface(X, Y, Z.imag, rstride=5, cstride=5, edgecolor='k', cmap=colormap, norm=divnorm1)
 	ax2.set_xlabel(r"$x$")
 	ax2.set_ylabel(r"$iy$")
 	ax2.set_zlabel(r"Im($f(x+iy)$)")
@@ -58,12 +62,7 @@ def complex_plot_2d_(X, Y, Z):
 	plt.tight_layout()
 
 if __name__=="__main__":
-	print("Input the upper bound of Binet numbers, n: ")
-	num_str = input()
-	if num_str == "":
-		num_str = "6"
-	num = float(num_str)
-	print(num_str)
+	num = 6 # Change this
 
 
 	####################################################
@@ -71,8 +70,8 @@ if __name__=="__main__":
 	####################################################
 	n_array = np.arange(0, num, 0.001)
 	binet_array = binet(n_array)
-	complex_plot_(n_array, binet_array, "$n$", "$f(n)$", "Positive numbers")	
-	
+	complex_plot_(n_array, binet_array, "$n$", "$f(n)$", "Positive numbers")
+
 	####################################################
 	# Negative Numbers
 	####################################################
@@ -97,6 +96,3 @@ if __name__=="__main__":
 	print("Binet Analysis Done!")
 
 	plt.show()
-	
-	
-	
